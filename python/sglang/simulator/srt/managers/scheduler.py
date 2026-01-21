24220d82fceabde35072c6ecebe8efd32dbda197
wjw187 <2157634909@qq.com>
2026-01-21 19:12:40 +0800

    fix import error

--- python/sglang/simulator/srt/managers/scheduler.py
+++ python/sglang/simulator/srt/managers/scheduler.py
@@ -7,6 +7,7 @@ import psutil
 import threading
 import torch
 import zmq
+from typing import Any
 from sglang.srt.utils import get_zmq_socket
 from sglang.srt.utils.torch_memory_saver_adapter import TorchMemorySaverAdapter
 from sglang.srt.server_args import PortArgs, ServerArgs, get_global_server_args
