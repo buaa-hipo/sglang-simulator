38f3cbcdc50c1879c771a0dfc6610236efb739e0
wjw187 <2157634909@qq.com>
2026-01-21 20:33:54 +0800

    print

--- python/sglang/simulator/profiler/profiler.py
+++ python/sglang/simulator/profiler/profiler.py
@@ -78,8 +78,8 @@ class Profiler:
                 else:
                     self.maybe_sleep_on_idle()
         finally:
-            if hasattr(profiler, "_fp") and profiler._fp:
-                profiler._fp.close()
+            if  self._fp:
+                self._fp.close()
 
     def maybe_sleep_on_idle(self):
         if self.idle_sleeper is not None:
